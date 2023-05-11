ae64c631cba580dcc138c27f6e184dc18dc78604
Taiga Nishino <e215736@ie.u-ryukyu.ac.jp>
2023-05-11 14:12:42 +0900

    fifth commit

--- regression.py
+++ regression.py
@@ -6,9 +6,10 @@ class LinearRegression:
     y = None
     
     def fit(self,x,y):
-        pass
+        temp = np.linalg.inv(np.dot(x.T,x))
+        self.theta = np.dot(np.dot(temp,x.T),y)
     def predict(self,x):
-        pass
+        return np.dot(x,self.theta)
     def score(self,x,y):
         pass
         
