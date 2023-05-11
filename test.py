c0bdc133820c3b267b3c83cd1a377d2d254e6e38
Taiga Nishino <e215736@ie.u-ryukyu.ac.jp>
2023-05-11 14:16:56 +0900

    sixth commit

--- test.py
+++ test.py
@@ -8,7 +8,15 @@ import regression
 import importlib
 importlib.reload(regression)
 model = regression.LinearRegression()
+
+#ver2
 model.fit(X,Y)
 print(model.theta)
+
+#ver3
 model.predict(X)
 print(model.predict(X))
+
+#ver4
+model.score(X,Y)
+print(model.score(X,Y))
